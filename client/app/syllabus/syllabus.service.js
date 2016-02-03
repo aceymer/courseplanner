e823a4c6e702b9f86974013c20068ea055e0f5f5
aceymer <ljb@easv.dk>
2016-02-03 23:04:08 +0100

    Added new service

---
+++ client/app/syllabus/syllabus.service.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('courseplannerApp')
+  .factory('SyllabusService', function($resource) {
+    return $resource('/api/syllabuses/:id',{
+        id: '@id'
+      });
+});
