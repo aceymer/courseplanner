5fbe090e09f231e32ed698594b463ea97abc5756
aceymer <ljb@easv.dk>
2016-02-04 09:17:58 +0100

    read all syllabuses and show

--- client/app/syllabus/syllabus.controller.js
+++ client/app/syllabus/syllabus.controller.js
@@ -2,6 +2,9 @@
 
 angular.module('courseplannerApp')
   .controller('SyllabusCtrl', function ($scope, SyllabusService) {
+    SyllabusService.query(function(syllabuses){
+        $scope.syllabuses = syllabuses;
+    });
     $scope.createSyllaus = function(){
       SyllabusService.save($scope.newSyllabus, function(syllabus) {
         console.log(syllabus);
--- server/api/syllabus/index.js
+++ server/api/syllabus/index.js
@@ -10,6 +10,6 @@ router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.delete('/:id', controller.destroy); 
 
 module.exports = router;
