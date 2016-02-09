0ee99197fd10055d72b1a5acd472c0e16ce30393
aceymer <ljb@easv.dk>
2016-02-09 21:19:09 +0100

    added socket.io

--- client/app/syllabus/syllabus.controller.js
+++ client/app/syllabus/syllabus.controller.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('courseplannerApp')
-  .controller('SyllabusCtrl', function ($scope, SyllabusService) {
+  .controller('SyllabusCtrl', function ($scope, SyllabusService, socket) {
     SyllabusService.query(function(syllabuses){
         $scope.syllabuses = syllabuses;
+        socket.syncUpdates('syllabus', $scope.syllabuses);
     });
     $scope.createSyllaus = function(){
       SyllabusService.save($scope.newSyllabus, function(syllabus) {
         console.log(syllabus);
       })
     }
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('syllabus');
+    });
   });
