a3ebc97e8f380bc7f625c047b18cae9775b79023
aceymer <ljb@easv.dk>
2016-02-09 22:39:24 +0100

    added media object and cleared newSyllabus

--- client/app/syllabus/syllabus.controller.js
+++ client/app/syllabus/syllabus.controller.js
@@ -8,7 +8,8 @@ angular.module('courseplannerApp')
     });
     $scope.createSyllaus = function(){
       SyllabusService.save($scope.newSyllabus, function(syllabus) {
-        console.log(syllabus);
+        $scope.newSyllabus = {};
+
       })
     }
 
