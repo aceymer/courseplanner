2b86ffab942c224dcfbbf9af10cf4a7186279251
aceymer <ljb@easv.dk>
2016-02-13 23:41:01 +0100

    Added new route (week)

--- client/app/syllabus/syllabus.controller.js
+++ client/app/syllabus/syllabus.controller.js
@@ -1,25 +1,34 @@
 'use strict';
 
 angular.module('courseplannerApp')
-  .controller('SyllabusCtrl', function ($scope, SyllabusService, socket) {
-    SyllabusService.query(function(syllabuses){
-        $scope.syllabuses = syllabuses;
-        socket.syncUpdates('syllabus', $scope.syllabuses);
+  .controller('SyllabusCtrl', function($scope, $state, SyllabusService, socket) {
+    SyllabusService.query(function(syllabuses) {
+      $scope.syllabuses = syllabuses;
+      socket.syncUpdates('syllabus', $scope.syllabuses);
     });
-    $scope.createSyllaus = function(){
+    $scope.createSyllaus = function() {
       SyllabusService.save($scope.newSyllabus, function(syllabus) {
         $scope.newSyllabus = {};
 
       })
     }
 
-    $scope.deleteSyllabus = function(syllabus){
-      SyllabusService.delete({id: syllabus._id}, function(syllabus) {
+    $scope.deleteSyllabus = function(syllabus) {
+      SyllabusService.delete({
+        id: syllabus._id
+      }, function(syllabus) {
         console.log("Syllabus deletred");
       });
     }
 
-    $scope.$on('$destroy', function () {
+    $scope.goToSyllabus = function(syllabus) {
+
+      $state.go('week', {
+        id: syllabus._id
+      });
+    }
+
+    $scope.$on('$destroy', function() {
       socket.unsyncUpdates('syllabus');
     });
   });
