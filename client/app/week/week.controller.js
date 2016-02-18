b214b1548d9fe30cfd392570690037031be9c068
aceymer <ljb@easv.dk>
2016-02-18 22:43:22 +0100

    Made delete dialog popup

--- client/app/week/week.controller.js
+++ client/app/week/week.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('courseplannerApp')
-  .controller('WeekCtrl', function ($scope, $stateParams, SyllabusService) {
+  .controller('WeekCtrl', function ($scope, $stateParams, $mdDialog, SyllabusService) {
     SyllabusService.get({id:$stateParams.id}, function(syllabus) {
       $scope.syllabus = syllabus;
     });
@@ -17,5 +17,34 @@ angular.module('courseplannerApp')
     $scope.undoWeekplanEdit = function(){
       $scope.editingWeekplan = undefined;
     }
+
+    $scope.deleteWeekPlan = function(weekplan, event) {
+      var confirm = $mdDialog.confirm()
+        .title('Delete Weekplan')
+        .textContent('Are you sure you want to delete the Weekplan')
+        .ariaLabel('Delete')
+        .targetEvent(event)
+        .openFrom('#right')
+        .ok('Please do it!')
+        .cancel('No I changed my mind');
+      $mdDialog.show(confirm).then(function() {
+        _.remove($scope.syllabus.weekplans, function(plan) {
+          return plan._id === $scope.editingWeekplan._id;
+        });
+
+        Syllabus.update({
+          id: $scope.syllabus._id
+        }, $scope.syllabus, function(syllabus) {
+          $scope.syllabus = syllabus;
+          var toast = $mdToast.simple()
+            .textContent('Weekplan Deleted')
+            .action('OK')
+            .highlightAction(false)
+            .position('top');
+          $mdToast.show(toast);
+          $scope.editingWeekplan = undefined;
+        });
+      });
+    };
 
   });
