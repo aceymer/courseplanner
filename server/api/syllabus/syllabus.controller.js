d2991347bc1150bb4dd842bb10829e91e8d96b44
aceymer <ljb@easv.dk>
2016-02-19 00:35:28 +0100

    Backend fix - lodash assign

--- server/api/syllabus/syllabus.controller.js
+++ server/api/syllabus/syllabus.controller.js
@@ -40,7 +40,7 @@ function handleEntityNotFound(res) {
 
 function saveUpdates(updates) {
   return function(entity) {
-    var updated = _.merge(entity, updates);
+    var updated = _.assign(entity, updates);
     return updated.saveAsync()
       .spread(updated => {
         return updated;
