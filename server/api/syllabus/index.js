159c37063403cbe1741a9f59adf9b30dc8449720
aceymer <ljb@easv.dk>
2016-02-28 22:23:39 +0100

    first security - endpoint

--- server/api/syllabus/index.js
+++ server/api/syllabus/index.js
@@ -2,14 +2,15 @@
 
 var express = require('express');
 var controller = require('./syllabus.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy); 
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
