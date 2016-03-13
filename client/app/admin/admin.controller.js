10a233dd61b0def70151ec013a365aef6569c074
lbilde <ljb@inno-tech.dk>
2016-03-13 20:33:38 +0100

    fixed minor issue and added user route

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -18,3 +18,27 @@ angular.module('courseplannerApp.admin')
   .controller('AdminController', AdminController);
 
 })();
+
+
+/*
+'use strict';
+
+angular.module('courseplannerApp.admin')
+  .controller('AdminController', function($scope, $state, User, Auth, socket) {
+    $scope.isAuthenticated = Auth.isLoggedIn;
+    $scope.users = User.query(function(users) {
+      $scope.users = users;
+      socket.syncUpdates('user', $scope.syllabuses);
+    });
+
+
+    $scope.delete = function(user) {
+      user.$remove();
+      this.users.splice(this.users.indexOf(user), 1);
+    };
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('user');
+    });
+  });
+*/
---
+++ client/app/user/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('courseplannerApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('user', {
+        url: '/user',
+        templateUrl: 'app/user/user.html',
+        controller: 'UserCtrl'
+      });
+  });
