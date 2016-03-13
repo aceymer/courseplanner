dda18b32b0f90dbfced925767008d03649b6813a
lbilde <ljb@inno-tech.dk>
2016-03-13 21:02:43 +0100

    added user list

--- client/app/user/user.controller.js
+++ client/app/user/user.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('courseplannerApp')
-  .controller('UserCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('UserCtrl', function ($scope, $state, socket, Auth, User) {
+    $scope.isAuthenticated = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+
+    User.query(function(users) {
+      $scope.users = users;
+      socket.syncUpdates('user', $scope.users);
+    });
+
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('user');
+    });
+
   });
