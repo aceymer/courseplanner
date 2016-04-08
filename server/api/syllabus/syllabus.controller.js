50ef56dbcf7ab7c077788a82f685568e344a2260
lbilde <ljb@inno-tech.dk>
2016-04-09 01:00:31 +0200

    added paging

--- server/api/syllabus/syllabus.controller.js
+++ server/api/syllabus/syllabus.controller.js
@@ -10,6 +10,7 @@
 'use strict';
 
 import _ from 'lodash';
+
 var Syllabus = require('./syllabus.model');
 
 function handleError(res, statusCode) {
@@ -61,10 +62,45 @@ function removeEntity(res) {
 
 // Gets a list of Syllabuss
 export function index(req, res) {
-  Syllabus.find({}).populate('owner', 'name email')
-    .execAsync()
-    .then(responseWithResult(res))
-    .catch(handleError(res));
+  //Create the query
+  var query = {};
+  if(req.query.search && req.query.search.length > 0){
+    query = {'title':new RegExp(req.query.search, 'i') };
+  }
+
+
+  //Make sure limit and page are numbers and above 1
+  if(!req.query.limit || parseFloat(req.query.limit) < 1){
+    req.query.limit = 25;
+  }
+  if(!req.query.page || parseFloat(req.query.page) < 1){
+    req.query.page = 1;
+  }
+
+  //Create the offset (ex. page = 1 and limit = 25 would result in 0 offset. page = 2 and limit = 25 would result in 25 offset.)
+  var offset = (req.query.page - 1) * req.query.limit;
+
+  //Testing if offset is bigger then result, if yes set offset to zero
+  Syllabus.count(query, function(err, count) {
+      if(offset > count){
+        offset = 0;
+      }
+
+      //Create object for pagination query
+      var options = {
+        select: 'title lecturer year owner',
+        sort: req.query.sortBy,
+        populate: {path: 'owner', select: 'name email'},
+        offset: offset,
+        limit: req.query.limit
+      };
+
+      //Do the actual pagination
+      Syllabus.paginate(query, options)
+        .then(responseWithResult(res))
+        .catch(handleError(res));
+  });
+
 }
 
 // Gets a single Syllabus from the DB
